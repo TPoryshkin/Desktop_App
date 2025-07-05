2ac65d391b2f886064d0a523e8feb71b4f02224e
peris <perishkin05@gmail.com>
2025-07-05 14:53:02 +0300

    class MenuItem and Program added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static List<MenuItem> items = new List<MenuItem>();
+    static decimal tipAmount = 0;
+}
+
+class MenuItem
+{
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+
+    public MenuItem(string description, decimal price)
+    {
+        Description = description;
+        Price = price;
+    }
+}
